a77c1eda8a5f39ef08c365c49547fd088a2bd950
Ryder <rider159159@gmail.com>
2023-05-08 16:35:51 +0800

    fix: fix userStore interface

--- src/interface/userStore/const.ts
+++ src/interface/userStore/const.ts
@@ -4,7 +4,16 @@ export const UserInfo:userInfo_interFace = {
   account: '',
   name: '',
   username: '',
-  gender: null,
-  isAllowedNotifications: true,
+  gender: 0,
+  email: '',
+  image: null,
+  address: null,
+  birthday: null,
+  customizedUrl: null,
   isSubscribed: true,
+  isAllowedNotifications:true,
+  website: null, // 個人網站,
+  youtube: null, // YT 網址
+  instagram: null,
+  facebook: null,
 }
