5aaa580405afaf5fa6ae21d8d9cd7d91133d0db7
Ryder <rider159159@gmail.com>
2023-07-02 01:47:12 +0800

    fix: fix style and cookie

--- src/components/common/FooterProposal.vue
+++ src/components/common/FooterProposal.vue
@@ -25,7 +25,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="fixed flex flex-col lg:flex-row lg:items-center lg:justify-between left-0 bottom-0 lg:h-80px b-t-line b-t-2 w-100vw bg-white px-6 py-4 lg:px-10">
+  <div class="fixed z-0 flex flex-col lg:flex-row lg:items-center lg:justify-between left-0 bottom-0 lg:h-80px b-t-line b-t-2 w-100vw bg-white px-6 py-4 lg:px-10">
     <a :href="`${VITE_FRONT_BUILD_URL}/proposal/${PROPOSAL_REF.customizedUrl}`" target="_blank" class="flex items-center lg:max-w-50%">
       <p class="mr-4"> {{ PROPOSAL_REF.name }}</p>
       <span class="mdi mdi-open-in-new text-xl"></span>
